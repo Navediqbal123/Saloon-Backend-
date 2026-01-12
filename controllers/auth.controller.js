0b98113399eefae28a83cb0f917248a4b8fdcaba
Navediqbal123 <navedahmad9012@gmail.com>
2026-01-12 18:59:25 +0530

    Create auth.controller.js

---
+++ controllers/auth.controller.js
@@ -0,0 +1,16 @@
+import supabase from "../config/supabase.js";
+
+export async function createProfile(req, res) {
+  const { role, name, phone } = req.body;
+
+  const { error } = await supabase.from("users").insert({
+    auth_id: req.user.id,
+    role,
+    name,
+    phone
+  });
+
+  if (error) return res.status(400).json(error);
+
+  res.json({ success: true });
+}
