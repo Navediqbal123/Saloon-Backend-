f668e943806133bc6fbcf05966b06f627051a563
Navediqbal123 <navedahmad9012@gmail.com>
2026-01-12 22:52:15 +0530

    Update barber.routes.js

--- routes/barber.routes.js
+++ routes/barber.routes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { registerBarber, approveBarber } from "../controllers/barber.controller.js";
+import { addService } from "../controllers/service.controller.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/register", authMiddleware, registerBarber);
+router.post("/add-service", authMiddleware, addService);
 router.post("/approve/:id", approveBarber); // admin use
 
 export default router;
