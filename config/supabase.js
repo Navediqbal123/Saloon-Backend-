e12b0054154a93ba9ecfb146a4675e0a3c718a18
Navediqbal123 <navedahmad9012@gmail.com>
2026-01-13 01:18:53 +0530

    Update supabase.js

--- config/supabase.js
+++ config/supabase.js
@@ -9,3 +9,6 @@ export const supabaseAdmin = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
+
+// Ye niche wali line add ki hai, isse error fix ho jayega
+export default supabaseAuth;
