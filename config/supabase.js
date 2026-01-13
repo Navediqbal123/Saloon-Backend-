daaae611c6e3a320b9e5694cad5dc1915e92db11
Navediqbal123 <navedahmad9012@gmail.com>
2026-01-13 15:51:50 +0530

    Update supabase.js

--- config/supabase.js
+++ config/supabase.js
@@ -1,14 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseAuth = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
-);
-
-export const supabaseAdmin = createClient(
+const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
-// Ye niche wali line add ki hai, isse error fix ho jayega
-export default supabaseAuth;
+export default supabase;
