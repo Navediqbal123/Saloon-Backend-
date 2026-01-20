f44ba0162f1b45ce8902bce75656007dd9e64c7a
Navediqbal123 <navedahmad9012@gmail.com>
2026-01-20 17:09:45 +0530

    Update booking.routes.js

--- routes/booking.routes.js
+++ routes/booking.routes.js
@@ -9,34 +9,16 @@ import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-/**
- * =========================
- * USER → Create booking
- * =========================
- */
+// USER → Create booking
 router.post("/create", authMiddleware, createBooking);
 
-/**
- * =========================
- * USER → My bookings
- * =========================
- */
+// USER → My bookings
 router.get("/my", authMiddleware, getMyBookings);
 
-/**
- * =========================
- * BARBER → My received bookings
- * (jitni booking us barber ke paas aayi)
- * =========================
- */
+// BARBER → My received bookings
 router.get("/barber", authMiddleware, getBarberBookings);
 
-/**
- * =========================
- * ADMIN → All bookings
- * (sab users + sab barbers)
- * =========================
- */
+// ADMIN → All bookings
 router.get("/all", authMiddleware, getAllBookings);
 
 export default router;
