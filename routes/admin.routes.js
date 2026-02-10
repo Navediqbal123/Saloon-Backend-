b49740f2b014d4f93474bb26f40f6708b4705548
Navediqbal123 <navedahmad9012@gmail.com>
2026-02-11 01:27:44 +0530

    Add route to approve barbers

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { getAllUsers } from "../controllers/admin.controller.js";
+import { getAllUsers, approveBarber } from "../controllers/admin.controller.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
+// Users ki list dekhne ke liye
 router.get("/users", authMiddleware, getAllUsers);
+
+// Barber approve karne aur role change karne ke liye
+router.post("/approve-barber", authMiddleware, approveBarber);
 
 export default router;
